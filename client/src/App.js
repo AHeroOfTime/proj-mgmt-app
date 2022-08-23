5d4a4c734f85242805d269370bdc1e0be1d6b247
Aaron Rodriguez <aaroner@yahoo.com>
2022-08-23 12:47:13 -0500

    starting on adding client modal UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Clients from './components/Clients';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import AddClientModal from './components/AddClientModal';
 
 // create a merge function for cache so you dont get the cache warning in apollo
 const cache = new InMemoryCache({
@@ -33,6 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className='container'>
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
